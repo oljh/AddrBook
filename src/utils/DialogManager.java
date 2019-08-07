2601e027d24d13804110ffcc8679ff98aa538912
oljh <https://github.com/oljh/MyCheese.git>
2019-08-07 19:50:11 +0300

    AddrBook - Fxml

--- src/utils/DialogManager.java
+++ src/utils/DialogManager.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Alert;
 public class DialogManager {
     public static void showInfoDialog(String title, String text){
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
+
     }
 
 }
